058de9bae864af4a7dc588b9d9e3296ffedde6ab
Akshat Gupta <akshatvg23@gmail.com>
2020-09-29 12:43:11 +0530

    mentors and student partners

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,6 +17,17 @@
     // Auto Init 
     M.AutoInit();
 
+    // Carousel
+    var elems = document.querySelectorAll('.carousel');
+    var options = {
+        fullWidth: false,
+        indicators: false
+    };
+    var instance = M.Carousel.init(elems, options);
+    setInterval(function () {
+        $('.carousel').carousel('next');
+    }, 5000);
+
 })(jQuery);
 
 console.clear();
